e7cb7af27df456ec775ef569b700586f9317ce60
jpleitao <jocaleitao93@gmail.com>
2013-11-01 17:02:59 +0000

    Generation of time series is looking better now

--- teste_gera_series.m
+++ teste_gera_series.m
@@ -2,25 +2,26 @@
 %% Function that generates a random signal, used as an input for a given system. This function returns the system's output for the generated input
 %%  number_data is the amount of data to generate
 %%%%
-function y=teste_gera_series(number_data)
+function [y,t,u]=teste_gera_series(number_data)
     load sys.mat
     
-    signal = rand();
-    random_number = rand();
+    step = 0.1;
+    limit = (number_data-1)*step;%So we generate "number_data" elements
+    random_number = rand(1,number_data);
     
-    if (signal > 0.5)
-       random_number = random_number + signal;
-    else
-        random_number = random_number - signal;
-    end     
+    temp = find(random_number>0.5);
+    %Y(find(Y<3)) = rand(length(find(Y<3)),1);
+    random_number(temp) = rand(length(temp),1);%This should work....
 
-    t = 0:0.01:number_data-1;
-    u = sin(0.01*rand()*t) + random_number;%input
+    t = 0:step:limit;
+    u = sin(0.01*t) + random_number*0.05;%input
     y = lsim(sys,u,t);
+    
     
     figure(1);
     plot(t,u,'b');
     hold on;
     plot(t,y,'r');
     legend('Generated System Input','System Output');
+    
 end
