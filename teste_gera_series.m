2a73e26e1ebe758957438db7729582708d2eb3b5
jpleitao <jocaleitao93@gmail.com>
2013-11-01 16:19:02 +0000

    Generated input for the system and corresponding output

--- teste_gera_series.m
+++ teste_gera_series.m
@@ -1,18 +1,26 @@
 %%%%
-%% Function that generates a random input signal to a system, with "number_data" values
+%% Function that generates a random signal, used as an input for a given system. This function returns the system's output for the generated input
+%%  number_data is the amount of data to generate
 %%%%
-function teste_gera_series(number_data)
+function y=teste_gera_series(number_data)
     load sys.mat
+    
+    signal = rand();
+    random_number = rand();
+    
+    if (signal > 0.5)
+       random_number = random_number + signal;
+    else
+        random_number = random_number - signal;
+    end     
 
-    random_number = rand() + 10;%Random offset
-
-    t = 0:number_data-1;
-    u = sin(0.05*t) + random_number;
-    figure(1);
+    t = 0:0.01:number_data-1;
+    u = sin(0.01*rand()*t) + random_number;%input
     y = lsim(sys,u,t);
-    plot(t,y);
-
-
-    figure(2);
-    plot(t,u);
+    
+    figure(1);
+    plot(t,u,'b');
+    hold on;
+    plot(t,y,'r');
+    legend('Generated System Input','System Output');
 end
