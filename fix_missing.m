cd0730f802b1d9ab8f348d23a8c22086378f1d80
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-11-17 03:40:37 +0000

    Resample isn't filling the last value if it is missing, warn the user we're going to get screwed

--- fix_missing.m
+++ fix_missing.m
@@ -2,4 +2,10 @@ function data = fix_missing(t, data)
     tser = timeseries(data,t);
     tser_fixed = resample(tser,t);
     data = tser_fixed.data;
+    
+    if sum(isnan(data)) > 0
+        data(end)=data(end-1);
+        disp('Problems are coming!');
+        pause;
+    end
 end
