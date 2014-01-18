2b294bb5bee5b1b150a096faeaf5f7f71ce43d47
João Ricardo Lourenço <jorl17.8@gmail.com>
2014-01-18 17:08:41 +0000

    Parsing files

---
+++ get_data_from_file.m
@@ -0,0 +1,37 @@
+function [status,t,data] = get_data_from_file (path)        
+    [status, filedata] = parse_file(path);
+    
+    if status == 0
+        t = 0; data = 0; return
+    end
+    
+    [numrows, numcols] = size(filedata);            
+    
+    if numrows == 2 && numcols == 1
+        t = filedata(1,:)';
+        data = filedata(2,:)';
+        status = 1;
+    elseif numcols == 2 && numrows==1
+        t = filedata(:,1);
+        data = filedata(:,2);
+        status = 1;
+    else
+        status = 0; t=0; data=0;
+    end
+end
+
+function [status,filedata] = parse_file(path)
+    [~, ~, ext] = fileparts(path);
+    if strcmp(ext,'.csv')        
+        filedata = csvread(path);
+        status = 1;
+    elseif strcmp(ext,'.xls') 
+        [filedata,~,~] = xlsread(path);
+        status = 1;
+    elseif strcmp(ext,'.xlsx') 
+        [filedata,~,~] = xlsread(path);
+        status = 1;
+    else
+        status = 0;
+    end
+end
